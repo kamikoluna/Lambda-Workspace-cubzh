74035b198870cc3c4517c5bd158a5af8c9320bbc
kamikoluna <90835140+kamikoluna@users.noreply.github.com>
2024-04-28 18:39:45 +0800

    merging into main.lua

--- Modules/systemrequire/system.lua
+++ Modules/systemrequire/system.lua
@@ -1,7 +1,8 @@
-local apis = {}
+--[[local apis = {}
   local api = require("system_api", System) 
   if api then 
     print("hi")
 end
   api:postSecret("hi", function() return error("hi") end)
 return apis
+]]--
