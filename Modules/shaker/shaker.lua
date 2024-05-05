72e9ecabb709fe5b20dcfae08c2f0d336530f706
kamikoluna <90835140+kamikoluna@users.noreply.github.com>
2024-05-05 19:20:53 +0800

    Update shaker.lua
    For some index and syntax correction

--- Modules/shaker/shaker.lua
+++ Modules/shaker/shaker.lua
@@ -1,23 +1,23 @@
 local shake = {}
 local clock = 0
 local FPSlimit = 30
-shake.new = function (y1, x1, z1, time1)
+shake.new = function (cam,y1, x1, z1, time1)
+ local currentposcam = cam.LocaPosition
 	for i = 1, time1 do
 		Timer(tick(), false, function()
             local x = math.random(-100,x1)/x1
             local y = math.random(-100,y1)/y1
             local z = math.random(-100,z1)/z1
-            Camera.LocalPosition = Number3(x,y,z)
+            cam.LocalPosition = Number3(x,y,z)
         end)
-		--print(i,x,y,z)
-		--game:GetService("RunService").RenderStepped:Wait()
 	end
-	Camera.LocalPosition = Number3(0,0,0)
+	cam.LocalPosition = Number3(0,0,0)
 end
 
-function tick(dt)
+function tick()
     local Start = os.clock()
     local DeltaTime = os.clock()-Start
+    return Deltatime
 end
 
 
