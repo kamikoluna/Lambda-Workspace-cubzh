54a925c4dd8faa20368fb6424720295394ce6e6e
kamikoluna <90835140+kamikoluna@users.noreply.github.com>
2024-05-06 15:41:15 +0800

    fixed shaker.lua (dam)
    its a little problem about os.clock is not build in.

--- Modules/shaker/shaker.lua
+++ Modules/shaker/shaker.lua
@@ -1,24 +1,21 @@
 local shake = {}
-local clock = 0
-local FPSlimit = 30
+
+shake.deltaTime = Object()
+shake.deltaTime.Tick = function(self, dt)
+  shake.dt = dt
+end
+
 shake.new = function (cam,y1, x1, z1, time1)
- local currentposcam = cam.LocaPosition
-	for i = 1, time1 do
-		Timer(tick(), false, function()
+ local currentposcam = cam.LocalPosition
+        for i = 1, time1 do
+                Timer(shake.dt*i, false, function()
             local x = math.random(-100,x1)/x1
             local y = math.random(-100,y1)/y1
             local z = math.random(-100,z1)/z1
             cam.LocalPosition = Number3(x,y,z)
         end)
-	end
-	cam.LocalPosition = Number3(0,0,0)
+        end
+        Timer(shake.dt*time1, false function() cam.LocalPosition = currentposcam end)
 end
-
-function tick()
-    local Start = os.clock()
-    local DeltaTime = os.clock()-Start
-    return Deltatime
-end
-
 
 return shake
